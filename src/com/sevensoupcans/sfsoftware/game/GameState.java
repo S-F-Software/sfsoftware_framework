b542419ceb16d4991c6f5c52866f4b1a716d3f7e
Steve Thompson <fuzmeister@sevensoupcans.com>
2021-08-25 15:37:39 -0400

    Added state decriptions to GameState enum.

--- src/com/sevensoupcans/sfsoftware/game/GameState.java
+++ src/com/sevensoupcans/sfsoftware/game/GameState.java
@@ -1,5 +1,19 @@
 package com.sevensoupcans.sfsoftware.game;
 
 public enum GameState {
-	INTRO, TITLE_SCREEN, INGAME, GAMEOVER, CREDITS;	
+	INTRO("Intro"), TITLE_SCREEN("Title Screen"), INGAME("In Game"), GAMEOVER("Game Over"), CREDITS("Credits");
+	
+	private final String gameStateDescription;
+	
+	GameState(String gameStateDescription) {
+		this.gameStateDescription = gameStateDescription;
+	}
+
+	public String getStateDescription() {
+		return gameStateDescription;
+	}
+	
+	public String getStateName() {
+		return name();
+	}
 }
